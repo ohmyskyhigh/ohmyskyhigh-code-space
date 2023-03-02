99809cc9151aec831b808301d7dfc56965dcab2d
Abdul Rehman <mehars.6925@gmail.com>
2023-03-02 20:00:17 +0500

    removed comments

--- src/main/index.ts
+++ src/main/index.ts
@@ -29,21 +29,11 @@ const createWindow = (): void => {
 
   mainWindow.webContents.once("dom-ready", async () => {
     mainWindow.webContents.openDevTools();
-    //   await installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
-    //     .then((name) => console.log(`Added Extension:  ${name}`))
-    //     .catch((err) => console.log("An error occurred: ", err))
-    //     .finally(() => {});
   });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
@@ -51,12 +41,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
